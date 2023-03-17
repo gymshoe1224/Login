deaaab9f182631402dde820a836c79b22e8776ba
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2023-03-17 09:28:22 -0500

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Chris Markiewicz on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
